dc100f7b17c222d7d9a5ff4292915f2ab2f3769b
David Parker <info@redkeydesigns.com>
2025-03-16 14:55:44 -0400

    Deleting unnecessary comments

--- src/commands/metrics.rs
+++ src/commands/metrics.rs
@@ -45,7 +45,6 @@ fn execute_metrics_command(
     Ok(())
 }
 
-// This should be deleted -- this is a test
 fn initialize_metrics_collector(parallel_enabled: bool) -> MetricsCollector {
     MetricsCollector::new().enable_parallel_processing(parallel_enabled)
 }
